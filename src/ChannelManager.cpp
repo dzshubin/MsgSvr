26e7abb14e920629f9dc840df971b4caa10467d0
shubin <dzshubin@live.com>
2016-04-22 22:34:39 +0800

    频道管理器

---
+++ src/ChannelManager.cpp
@@ -0,0 +1,164 @@
+
+
+#include "ChannelManager.hpp"
+
+ChannelManager::ChannelManager()
+{
+
+
+}
+
+ChannelManager* ChannelManager::get_instance()
+{
+    static ChannelManager g_channel_mananger;
+    return &g_channel_mananger;
+}
+
+
+
+
+// 加入频道
+bool ChannelManager::JoinChannel(int ch_id, const IM::User& r_user_)
+{
+    auto it = m_mapChannels.find(ch_id);
+    if (it != m_mapChannels.end())
+    {
+        auto vec_it = std::find_if(it->second.m_users.begin(), it->second.m_users.end(),
+        [&] (const IM::User& user_)
+        {
+            return user_.id() == r_user_.id();
+        });
+        if (vec_it != it->second.m_users.end())
+        {
+            // 已经加入了频道
+            return false;
+        }
+        else
+        {
+            it->second.m_users.push_back(r_user_);
+        }
+    }
+    else
+    {
+        // 频道不存在
+        //m_mapChannels[ch_id].push_back(r_user_);
+        return false;
+    }
+
+    return true;
+}
+
+// 退出频道
+bool ChannelManager::ExitChannel(int ch_id, int64_t user_id_)
+{
+    auto it = m_mapChannels.find(ch_id);
+
+    if(it != m_mapChannels.end())
+    {
+        auto vec_it = std::find_if(it->second.m_users.begin(), it->second.m_users.end(),
+        [&] (const IM::User& user_)
+        {
+            return user_.id() == user_id_;
+        });
+
+        if (vec_it != it->second.m_users.end())
+        {
+            it->second.m_users.erase(vec_it);
+        }
+        else
+        {
+            // 玩家不是该频道的成员
+            return false;
+        }
+    }
+
+    else
+    {
+        // 频道不存在
+        return false;
+    }
+}
+
+
+
+bool ChannelManager::CreateChannel(int ch_id_, std::string name_)
+{
+    auto it = m_mapChannels.find(ch_id_);
+    if (it == m_mapChannels.end())
+    {
+        CHANNEL channel;
+        channel.m_name = name_;
+
+        m_mapChannels.insert(std::make_pair(ch_id_, channel));
+        return true;
+    }
+    else
+    {
+        // 频道已经存在
+        return false;
+    }
+}
+
+
+bool ChannelManager::RemoveChannel(int ch_id_)
+{
+    return false;
+}
+
+
+
+void ChannelManager::LoadChannelBaseInfo(int64_t user_id_, IM::ChannelBaseInfo& base_info_)
+{
+    auto it = m_mapChannels.cbegin();
+
+    for(; it != m_mapChannels.cend(); ++it)
+    {
+        IM::ChannelBase* pCB = base_info_.add_channel_base();
+
+        pCB->set_channel_id(it->first);
+        pCB->set_channel_name(it->second.m_name);
+
+        auto res_it = std::find_if(it->second.m_users.cbegin(), it->second.m_users.cend(),
+        [&](const IM::User& user_)
+        {
+            return user_.id() == user_id_;
+        });
+
+        bool result = (res_it == it->second.m_users.end() ? false : true);
+        pCB->set_is_inside(result);
+
+    }
+
+
+}
+
+void ChannelManager::LoadChannelMembers(IM::ChannelMembersInfo& members)
+{
+    auto it = m_mapChannels.cbegin();
+
+    for(; it != m_mapChannels.end(); ++it)
+    {
+        IM::ChannelMember *pMmember = members.add_channel_member();
+        pMmember->set_channel_id(it->first);
+
+        std::vector<IM::User> vec_users = it->second.m_users;
+        auto user_it = vec_users.cbegin();
+        for(; user_it != vec_users.cend(); ++user_it)
+        {
+            IM::User* pUser = pMmember->add_users();
+
+            pUser->set_id(user_it->id());
+            pUser->set_name(user_it->name());
+            pUser->set_nick_name(user_it->nick_name());
+            pUser->set_sex(user_it->sex());
+        }
+    }
+
+
+
+}
+
+
+
+
+
---
+++ src/ChannelManager.hpp
@@ -0,0 +1,53 @@
+#ifndef CHANNELMANAGER_HPP_INCLUDED
+#define CHANNELMANAGER_HPP_INCLUDED
+
+
+
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "contacts.pb.h"
+#include "channel_base_info.pb.h"
+#include "channel_members.pb.h"
+
+struct CHANNEL
+{
+    std::string m_name;
+    std::vector<IM::User> m_users;
+};
+
+
+// 频道管理器
+class ChannelManager
+{
+
+public:
+    static ChannelManager* get_instance();
+
+
+    // 创建频道
+    bool CreateChannel(int, std::string);
+    // 删除频道
+    bool RemoveChannel(int);
+    // 加入频道
+    bool JoinChannel(int, const IM::User&);
+    // 退出频道
+    bool ExitChannel(int, int64_t);
+    // 加载频道基本信息
+    void LoadChannelBaseInfo(int64_t, IM::ChannelBaseInfo&);
+    // 加载频道成员信息
+    void LoadChannelMembers(IM::ChannelMembersInfo&);
+
+private:
+    ChannelManager();
+
+private:
+    // map<频道号，频道信息>
+    std::map<int, CHANNEL>  m_mapChannels;
+};
+
+
+
+#endif // CHANNELMANAGER_HPP_INCLUDED
